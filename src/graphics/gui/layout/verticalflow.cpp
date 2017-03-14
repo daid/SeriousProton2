102c28d1b891510f46130902d5a3ac97ce451a59
daid <daid303@gmail.com>
2017-03-14 08:37:07 +0100

    Add vertical flow layout.

---
+++ src/graphics/gui/layout/verticalflow.cpp
@@ -0,0 +1,30 @@
+#include <sp2/graphics/gui/layout/verticalflow.h>
+#include <sp2/graphics/gui/widget/widget.h>
+
+namespace sp {
+namespace gui {
+
+SP_REGISTER_LAYOUT("verticalflow", VerticalFlowLayout);
+
+void VerticalFlowLayout::update(P<Container> container, const sf::FloatRect& rect)
+{
+    float y = rect.top;
+    float x0 = rect.left;
+    float x1 = rect.left;
+    for(Widget* w : container->children)
+    {
+        float height = w->layout.size.y + w->layout.margin_top + w->layout.margin_bottom;
+        if (y + height > rect.top + rect.height)
+        {
+            x0 = x1;
+            y = rect.top;
+        }
+        sf::FloatRect r(x0, y, rect.width, height);
+        basicLayout(r, w);
+        x1 = std::max(x1, w->layout.rect.left + w->layout.rect.width + w->layout.margin_right);
+        y += height;
+    }
+}
+
+};//!namespace gui
+};//!namespace sp
