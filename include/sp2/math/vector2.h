74b36d53a24bbfe5da3b391677e8f682c9762fb7
daid <daid303@gmail.com>
2018-03-11 14:10:49 +0100

    Replace de SFML vector2 and vector3 with our own versions, slightly decoupling from sfml more.

---
+++ include/sp2/math/vector2.h
@@ -0,0 +1,79 @@
+#ifndef SP2_MATH_VECTOR2_H
+#define SP2_MATH_VECTOR2_H
+
+#include <cmath>
+#include <ostream>
+
+namespace sp {
+
+template<typename T> class Vector2
+{
+public:
+    T x, y;
+    
+    inline Vector2()
+    : x(0), y(0)
+    {
+    }
+    
+    inline Vector2(T x, T y)
+    : x(x), y(y)
+    {
+    }
+    
+    template <typename T2> explicit inline Vector2(const Vector2<T2>& v)
+    : x(v.x), y(v.y)
+    {
+    }
+
+    inline T length()
+    {
+        return std::sqrt(x * x + y * y);
+    }
+    
+    inline Vector2<T> normalized()
+    {
+        return *this / length();
+    }
+    
+    inline T cross(const Vector2<T>& v)
+    {
+        return x * v.y - y * v.x;
+    }
+
+    inline T dot(const Vector2<T>& v)
+    {
+        return x * v.x + y * v.y;
+    }
+
+    T angle()
+    {
+        return std::atan2(y, x) / 3.14159265358979323846 * 180.0f;
+    }
+};
+
+template <typename T> static inline Vector2<T> operator -(const Vector2<T>& a) { return Vector2<T>(-a.x, -a.y); }
+template <typename T> static inline Vector2<T> operator +(const Vector2<T>& a, const Vector2<T>& b) { return Vector2<T>(a.x+b.x, a.y+b.y); }
+template <typename T> static inline Vector2<T> operator -(const Vector2<T>& a, const Vector2<T>& b) { return Vector2<T>(a.x-b.x, a.y-b.y); }
+template <typename T> static inline Vector2<T> operator *(const Vector2<T>& a, const T b) { return Vector2<T>(a.x*b, a.y*b); }
+template <typename T> static inline Vector2<T> operator /(const Vector2<T>& a, const T b) { return Vector2<T>(a.x/b, a.y/b); }
+template <typename T> static inline void operator +=(Vector2<T>& a, const Vector2<T>& b) { a.x += b.x; a.y += b.y; }
+template <typename T> static inline void operator -=(Vector2<T>& a, const Vector2<T>& b) { a.x -= b.x; a.y -= b.y; }
+template <typename T> static inline void operator *=(Vector2<T>& a, const T b) { a.x *= b; a.y *= b; }
+template <typename T> static inline void operator /=(Vector2<T>& a, const T b) { a.x /= b; a.y /= b; }
+template <typename T> static inline bool operator ==(const Vector2<T>& a, const Vector2<T>& b) { return a.x == b.x && a.y == b.y; }
+template <typename T> static inline bool operator !=(const Vector2<T>& a, const Vector2<T>& b) { return a.x != b.x || a.y != b.y; }
+
+typedef Vector2<float> Vector2f;
+typedef Vector2<double> Vector2d;
+typedef Vector2<int> Vector2i;
+
+template <typename T> static inline std::ostream& operator<<(std::ostream& stream, const Vector2<T> v)
+{
+    stream << v.x << "," << v.y;
+    return stream;
+}
+
+};//!namespace sp
+
+#endif//SP2_MATH_VECTOR_H
---
+++ include/sp2/math/vector3.h
@@ -0,0 +1,69 @@
+#ifndef SP2_MATH_VECTOR3_H
+#define SP2_MATH_VECTOR3_H
+
+#include <cmath>
+#include <ostream>
+
+namespace sp {
+
+template<typename T> class Vector3
+{
+public:
+    T x, y, z;
+    
+    inline Vector3()
+    : x(0), y(0), z(0)
+    {
+    }
+    
+    inline Vector3(T x, T y, T z)
+    : x(x), y(y), z(z)
+    {
+    }
+    
+    template <typename T2> explicit inline Vector3(const Vector3<T2>& v)
+    : x(v.x), y(v.y), z(v.z)
+    {
+    }
+
+    inline T length()
+    {
+        return std::sqrt(x * x + y * y + z * z);
+    }
+    
+    inline Vector3<T> normalized()
+    {
+        return *this / length();
+    }
+    
+    inline T dot(const Vector3<T>& v)
+    {
+        return x * v.x + y * v.y + z * v.z;
+    }
+};
+
+template <typename T> static inline Vector3<T> operator -(const Vector3<T>& a) { return Vector3<T>(-a.x, -a.y, -a.z); }
+template <typename T> static inline Vector3<T> operator +(const Vector3<T>& a, const Vector3<T>& b) { return Vector3<T>(a.x+b.x, a.y+b.y, a.z+b.z); }
+template <typename T> static inline Vector3<T> operator -(const Vector3<T>& a, const Vector3<T>& b) { return Vector3<T>(a.x-b.x, a.y-b.y, a.z-b.z); }
+template <typename T> static inline Vector3<T> operator *(const Vector3<T>& a, const T b) { return Vector3<T>(a.x*b, a.y*b, a.z*b); }
+template <typename T> static inline Vector3<T> operator /(const Vector3<T>& a, const T b) { return Vector3<T>(a.x/b, a.y/b, a.z/b); }
+template <typename T> static inline void operator +=(Vector3<T>& a, const Vector3<T>& b) { a.x += b.x; a.y += b.y; a.z += b.z; }
+template <typename T> static inline void operator -=(Vector3<T>& a, const Vector3<T>& b) { a.x -= b.x; a.y -= b.y; a.z -= b.z; }
+template <typename T> static inline void operator *=(Vector3<T>& a, const T b) { a.x *= b; a.y *= b; a.z *= b; }
+template <typename T> static inline void operator /=(Vector3<T>& a, const T b) { a.x /= b; a.y /= b; a.z /= b; }
+template <typename T> static inline bool operator ==(const Vector3<T>& a, const Vector3<T>& b) { return a.x == b.x && a.y == b.y && a.z == b.z; }
+template <typename T> static inline bool operator !=(const Vector3<T>& a, const Vector3<T>& b) { return a.x != b.x || a.y != b.y || a.z != b.z; }
+
+typedef Vector3<float> Vector3f;
+typedef Vector3<double> Vector3d;
+typedef Vector3<int> Vector3i;
+
+template <typename T> static inline std::ostream& operator<<(std::ostream& stream, const Vector3<T> v)
+{
+    stream << v.x << "," << v.y << "," << v.z;
+    return stream;
+}
+
+};//!namespace sp
+
+#endif//SP2_MATH_VECTOR_H
