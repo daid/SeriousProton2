ad381d0ce91faabd447a1323768657b02dae35c1
daid <daid303@gmail.com>
2018-10-09 17:20:42 +0200

    Add 3D collision mesh.

---
+++ include/sp2/collision/3d/mesh.h
@@ -0,0 +1,35 @@
+#ifndef SP2_COLLISION_3D_MESH_H
+#define SP2_COLLISION_3D_MESH_H
+
+#include <vector>
+
+#include <sp2/collision/3d/shape.h>
+#include <sp2/math/vector3.h>
+
+class btTriangleIndexVertexArray;
+
+namespace sp {
+namespace collision {
+
+/**
+    NOTE: Unlike the other collision objects, the Mesh3D needs to remain in memory as long as any object is using this as collision shape.
+            else you will get a crash from the collision engine, as it will try to access the stored vertices/indices.
+ */
+class Mesh3D : public Shape3D
+{
+public:
+    Mesh3D(std::vector<Vector3f>&& vertices, std::vector<int>&& indices);
+
+private:
+    virtual btCollisionShape* createShape() const override;
+    
+    btTriangleIndexVertexArray* triangle_index_array = nullptr;
+
+    std::vector<Vector3f> vertices;
+    std::vector<int> indices;
+};
+
+};//namespace collision
+};//namespace sp
+
+#endif//SP2_COLLISION_3D_MESH_H
---
+++ src/collision/3d/mesh.cpp
@@ -0,0 +1,21 @@
+#include <sp2/collision/3d/mesh.h>
+#include <sp2/logging.h>
+
+#include <btBulletDynamicsCommon.h>
+
+namespace sp {
+namespace collision {
+
+Mesh3D::Mesh3D(std::vector<Vector3f>&& vertices, std::vector<int>&& indices)
+: vertices(vertices), indices(indices)
+{
+    triangle_index_array = new btTriangleIndexVertexArray(this->indices.size() / 3, const_cast<int*>(&this->indices[0]), sizeof(int) * 3, this->vertices.size(), &this->vertices[0].x, sizeof(sp::Vector3f));
+}
+
+btCollisionShape* Mesh3D::createShape() const
+{
+    return new btBvhTriangleMeshShape(triangle_index_array, false);
+}
+
+};//namespace collision
+};//namespace sp
