73e46c718dfcf14cc5d1545d98a01d7c8f621d4b
daid <daid303@gmail.com>
2018-11-01 17:31:17 +0100

    Add contains function to rect.

--- include/sp2/math/rect.h
+++ include/sp2/math/rect.h
@@ -13,6 +13,11 @@ public:
     Rect(Vector2<T> position, Vector2<T> size) : position(position), size(size) {}
     Rect(T x, T y, T w, T h) : position(x, y), size(w, h) {}
     
+    bool contains(Vector2<T> p)
+    {
+        return p.x >= position.x && p.x <= position.x + size.x && p.y >= position.y && p.y <= position.y + size.y;
+    }
+    
     Vector2<T> position;
     Vector2<T> size;
 };
