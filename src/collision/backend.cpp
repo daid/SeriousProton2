6e0d6fcae58fdac5c44e824919c13bd94818834c
daid <daid303@gmail.com>
2018-12-04 20:12:06 +0100

    Add missing files of latest commit.

---
+++ src/collision/backend.cpp
@@ -0,0 +1,19 @@
+#include <sp2/collision/backend.h>
+#include <sp2/scene/node.h>
+
+
+namespace sp {
+namespace collision {
+
+void Backend::modifyPositionByPhysics(Node* node, sp::Vector2d position, double rotation)
+{
+    node->modifyPositionByPhysics(position, rotation);
+}
+
+void Backend::modifyPositionByPhysics(Node* node, sp::Vector3d position, Quaterniond rotation)
+{
+    node->modifyPositionByPhysics(position, rotation);
+}
+
+};//namespace collision
+};//namespace sp
---
+++ src/collision/shape.cpp
@@ -0,0 +1,30 @@
+#include <sp2/collision/shape.h>
+#include <sp2/scene/node.h>
+#include <sp2/scene/scene.h>
+
+
+namespace sp {
+namespace collision {
+
+Backend* Shape::getCollisionBackend(Node* node) const
+{
+    return node->scene->collision_backend;
+}
+
+void Shape::setCollisionBackend(Node* node, Backend* backend) const
+{
+    node->scene->collision_backend = backend;
+}
+
+void* Shape::getCollisionBody(Node* node) const
+{
+    return node->collision_body;
+}
+
+void Shape::setCollisionBody(Node* node, void* body) const
+{
+    node->collision_body = body;
+}
+
+};//namespace collision
+};//namespace sp
