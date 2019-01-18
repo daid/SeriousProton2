a798744fa294c6205a76f00f64ce4c39d7e9bc1b
daid <daid303@gmail.com>
2019-01-18 21:25:52 +0100

    Add the missing textinput header.

---
+++ include/sp2/io/textinput.h
@@ -0,0 +1,22 @@
+#ifndef SP2_IO_TEXTINPUT_H
+#define SP2_IO_TEXTINPUT_H
+
+namespace sp {
+
+enum class TextInputEvent
+{
+    Left,
+    Right,
+    Up,
+    Down,
+    LineStart,
+    LineEnd,
+    TextStart,
+    TextEnd,
+    Delete,
+    Backspace
+};
+
+};//namespace sp
+
+#endif//SP2_IO_TEXTINPUT_H
