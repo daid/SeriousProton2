7e358878bc306df8d5addb920cdd71cdf7f22f25
daid <daid303@gmail.com>
2019-07-03 22:04:33 +0200

    Add a virtual touch button layer, which is handy on android to create areas of the screen that are bound to keybindings.

---
+++ include/sp2/io/virtualTouchKeys.h
@@ -0,0 +1,55 @@
+#ifndef SP2_IO_VIRTUAL_TOUCH_KEYS_H
+#define SP2_IO_VIRTUAL_TOUCH_KEYS_H
+
+#include <sp2/graphics/graphicslayer.h>
+
+
+namespace sp {
+namespace io {
+
+class Keybinding;
+
+/**
+    Class to handle areas of the screen as virtual keybinding keys.
+    This is most useful on android where the input options are limited, but parts of the screen
+    can be assigned to keysbindings.
+    
+    There are 3 types of bindings:
+    * Simple keys, on/off pressed or not.
+    * TODO: Single direction "joystick" like axis.
+    * TODO: 2 direction "joystick" bound to 2 different keybindings.
+ */
+class VirtualTouchKeyLayer : public sp::GraphicsLayer
+{
+public:
+    VirtualTouchKeyLayer(int priority);
+    
+    void addButton(const Rect2f& area, Keybinding& key);
+    void addButton(const Rect2f& area, int virtual_key_index);
+
+    virtual void render(RenderQueue& queue) override;
+    virtual bool onPointerDown(io::Pointer::Button button, Vector2d position, int id) override;
+    virtual void onPointerDrag(Vector2d position, int id) override;
+    virtual void onPointerUp(Vector2d position, int id) override;
+    virtual void onTextInput(const string& text) override;
+    virtual void onTextInput(TextInputEvent e) override;
+private:
+    int getIndexForKey(Keybinding& key);
+    Vector2f convertPosition(const Vector2d& position) const;
+
+    class VirtualButton
+    {
+    public:
+        Rect2f area;
+        int virtual_key_index;
+    };
+
+    std::vector<VirtualButton> buttons;
+    std::unordered_map<int, int> active_pointers;
+};
+
+};//namespace io
+};//namespace sp
+
+
+#endif//SP2_IO_VIRTUAL_TOUCH_KEYS_H
---
+++ src/io/virtualTouchKeys.cpp
@@ -0,0 +1,87 @@
+#include <sp2/io/virtualTouchKeys.h>
+#include <sp2/io/keybinding.h>
+
+
+namespace sp {
+namespace io {
+
+static int virtual_button_counter = 0;
+
+VirtualTouchKeyLayer::VirtualTouchKeyLayer(int priority)
+: sp::GraphicsLayer(priority)
+{
+}
+
+void VirtualTouchKeyLayer::render(RenderQueue& queue) {}
+
+bool VirtualTouchKeyLayer::onPointerDown(io::Pointer::Button button, Vector2d position, int id)
+{
+    for(unsigned int n=0; n<buttons.size(); n++)
+    {
+        if (buttons[n].area.contains(convertPosition(position)))
+        {
+            active_pointers[id] = n;
+            Keybinding::setVirtualKey(buttons[n].virtual_key_index, 1);
+            return true;
+        }
+    }
+    return false;
+}
+
+void VirtualTouchKeyLayer::onPointerDrag(Vector2d position, int id)
+{
+    auto it = active_pointers.find(id);
+    if (it != active_pointers.end())
+    {
+        if (buttons[it->second].area.contains(convertPosition(position)))
+            return;
+        Keybinding::setVirtualKey(buttons[it->second].virtual_key_index, 0);
+    }
+    for(unsigned int n=0; n<buttons.size(); n++)
+    {
+        if (buttons[n].area.contains(convertPosition(position)))
+        {
+            active_pointers[id] = n;
+            Keybinding::setVirtualKey(buttons[n].virtual_key_index, 1);
+            return;
+        }
+    }
+}
+
+void VirtualTouchKeyLayer::onPointerUp(Vector2d position, int id)
+{
+    auto it = active_pointers.find(id);
+    if (it != active_pointers.end())
+        Keybinding::setVirtualKey(buttons[it->second].virtual_key_index, 0);
+}
+
+void VirtualTouchKeyLayer::onTextInput(const string& text) {}
+void VirtualTouchKeyLayer::onTextInput(TextInputEvent e) {}
+
+
+void VirtualTouchKeyLayer::addButton(const Rect2f& area, Keybinding& key)
+{
+    addButton(area, getIndexForKey(key));
+}
+
+void VirtualTouchKeyLayer::addButton(const Rect2f& area, int virtual_key_index)
+{
+    buttons.emplace_back();
+    VirtualButton& vb = buttons.back();
+    vb.area = area;
+    vb.virtual_key_index = virtual_key_index;
+}
+
+int VirtualTouchKeyLayer::getIndexForKey(Keybinding& key)
+{
+    key.addKey("virtual:" + string(virtual_button_counter));
+    return virtual_button_counter++;
+}
+
+Vector2f VirtualTouchKeyLayer::convertPosition(const Vector2d& position) const
+{
+    return Vector2f(position.x / 2.0 + 0.5, position.y / -2.0 + 0.5);
+}
+
+};//namespace io
+};//namespace sp
