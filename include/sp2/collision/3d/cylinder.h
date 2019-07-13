9201472bb097e93e4e71ee7af8fbb3910ab289d6
daid <daid303@gmail.com>
2019-07-13 20:34:04 +0200

    Add 3d collision cylinder

---
+++ include/sp2/collision/3d/cylinder.h
@@ -0,0 +1,29 @@
+#ifndef SP2_COLLISION_3D_CYLINDER_H
+#define SP2_COLLISION_3D_CYLINDER_H
+
+#include <sp2/collision/3d/shape.h>
+
+namespace sp {
+namespace collision {
+
+class Cylinder3D : public Shape3D
+{
+public:
+    enum class Direction
+    {
+        X, Y, Z
+    };
+
+    Cylinder3D(Direction direction, double radius, double height);
+private:
+    Direction direction;
+    double radius;
+    double height;
+
+    virtual btCollisionShape* createShape() const override;
+};
+
+};//namespace collision
+};//namespace sp
+
+#endif//SP2_COLLISION_3D_CYLINDER_H
---
+++ src/collision/3d/cylinder.cpp
@@ -0,0 +1,28 @@
+#include <sp2/collision/3d/cylinder.h>
+
+#include <btBulletDynamicsCommon.h>
+
+namespace sp {
+namespace collision {
+
+Cylinder3D::Cylinder3D(Direction direction, double radius, double height)
+: direction(direction), radius(radius), height(height)
+{
+}
+
+btCollisionShape* Cylinder3D::createShape() const
+{
+    switch(direction)
+    {
+    case Direction::X:
+        return new btCylinderShapeX(btVector3(height / 2.0, radius, radius));
+    case Direction::Y:
+        return new btCylinderShape(btVector3(radius, height / 2.0, radius));
+    case Direction::Z:
+    default:
+        return new btCylinderShapeZ(btVector3(radius, radius, height / 2.0));
+    }
+}
+
+};//namespace collision
+};//namespace sp
