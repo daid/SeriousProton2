2d8f0d293a08bf4cf01e442deb46cd373d43819f
Daid <daid303@gmail.com>
2019-08-15 13:08:43 +0000

    Add support for exe and android icon by means of the SP2_ICON cmake variable.

--- SeriousProton2.include.cmake
+++ SeriousProton2.include.cmake
@@ -2,6 +2,17 @@ cmake_minimum_required(VERSION 3.6.0)
 
 set(SERIOUS_PROTON2_BASE_DIR ${CMAKE_CURRENT_LIST_DIR})
 
+if(WIN32)
+    set(CPACK_GENERATOR NSIS ZIP)
+    find_package(Git)
+    if(GIT_FOUND)
+        execute_process(COMMAND ${GIT_EXECUTABLE} describe --always OUTPUT_VARIABLE GIT_VERSION WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}" OUTPUT_STRIP_TRAILING_WHITESPACE)
+        set(CPACK_PACKAGE_VERSION "${GIT_VERSION}")
+    endif()
+    include(CPack)
+endif()
+
+
 macro(serious_proton2_executable EXECUTABLE_NAME)
     set(CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/cmake" ${CMAKE_MODULE_PATH})
     find_package(SDL2 REQUIRED)
@@ -23,7 +34,7 @@ macro(serious_proton2_executable EXECUTABLE_NAME)
     if(NOT UNIX)
         list(FILTER SP2_SOURCES EXCLUDE REGEX .*/unix/.*)
     endif()
-    
+
     if(NOT CMAKE_BUILD_TYPE)
         set(CMAKE_BUILD_TYPE "Release" CACHE STRING "Choose the type of build, options are: Debug Release RelWithDebInfo MinSizeRel." FORCE)
     endif()
@@ -72,6 +83,63 @@ macro(serious_proton2_executable EXECUTABLE_NAME)
     target_include_directories(sp2freetype PUBLIC "${SERIOUS_PROTON2_BASE_DIR}/extlibs/freetype-2.9/include")
     target_compile_definitions(sp2freetype PRIVATE "-DFT2_BUILD_LIBRARY")
 
+    if(SP2_ICON)
+        find_program(IMAGE_CONVERT convert)
+        if("${IMAGE_CONVERT}" STREQUAL "IMAGE_CONVERT-NOTFOUND")
+            message(WARNING "ImageMagick not found, cannot build icons. Which is fine for test builds. But release builds are better with icons.")
+        elseif(WIN32)
+            add_custom_command(
+                OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/icon.ico"
+                COMMAND "${IMAGE_CONVERT}" ARGS "${SP2_ICON}" -resize 256x256 -define icon:auto-resize="256,128,96,64,48,32,16" "${CMAKE_CURRENT_BINARY_DIR}/icon.ico"
+                WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+                DEPENDS "${SP2_ICON}")
+            file(GENERATE
+                OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/win32.rc"
+                CONTENT "id ICON \"icon.ico\"")
+            set_source_files_properties(
+                "${CMAKE_CURRENT_BINARY_DIR}/win32.rc"
+                PROPERTIES OBJECT_DEPENDS "${CMAKE_CURRENT_BINARY_DIR}/icon.ico")
+            list(APPEND SP2_SOURCES "${CMAKE_CURRENT_BINARY_DIR}/win32.rc")
+        elseif(ANDROID)
+            file(MAKE_DIRECTORY
+                "${CMAKE_CURRENT_BINARY_DIR}/android_resources/mipmap-mdpi"
+                "${CMAKE_CURRENT_BINARY_DIR}/android_resources/mipmap-hdpi"
+                "${CMAKE_CURRENT_BINARY_DIR}/android_resources/mipmap-xhdpi"
+                "${CMAKE_CURRENT_BINARY_DIR}/android_resources/mipmap-xxhdpi"
+                "${CMAKE_CURRENT_BINARY_DIR}/android_resources/mipmap-xxxhdpi")
+            add_custom_command(
+                OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/android_resources/mipmap-mdpi/ic_launcher.png"
+                COMMAND "${IMAGE_CONVERT}" ARGS "${SP2_ICON}" -resize 48x48 "${CMAKE_CURRENT_BINARY_DIR}/android_resources/mipmap-mdpi/ic_launcher.png"
+                WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+                DEPENDS "${SP2_ICON}")
+            add_custom_command(
+                OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/android_resources/mipmap-hdpi/ic_launcher.png"
+                COMMAND "${IMAGE_CONVERT}" ARGS "${SP2_ICON}" -resize 72x72 "${CMAKE_CURRENT_BINARY_DIR}/android_resources/mipmap-hdpi/ic_launcher.png"
+                WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+                DEPENDS "${SP2_ICON}")
+            add_custom_command(
+                OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/android_resources/mipmap-xhdpi/ic_launcher.png"
+                COMMAND "${IMAGE_CONVERT}" ARGS "${SP2_ICON}" -resize 96x96 "${CMAKE_CURRENT_BINARY_DIR}/android_resources/mipmap-xhdpi/ic_launcher.png"
+                WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+                DEPENDS "${SP2_ICON}")
+            add_custom_command(
+                OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/android_resources/mipmap-xxhdpi/ic_launcher.png"
+                COMMAND "${IMAGE_CONVERT}" ARGS "${SP2_ICON}" -resize 144x144 "${CMAKE_CURRENT_BINARY_DIR}/android_resources/mipmap-xxhdpi/ic_launcher.png"
+                WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+                DEPENDS "${SP2_ICON}")
+            add_custom_command(
+                OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/android_resources/mipmap-xxxhdpi/ic_launcher.png"
+                COMMAND "${IMAGE_CONVERT}" ARGS "${SP2_ICON}" -resize 192x192 "${CMAKE_CURRENT_BINARY_DIR}/android_resources/mipmap-xxxhdpi/ic_launcher.png"
+                WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+                DEPENDS "${SP2_ICON}")
+            list(APPEND ANDROID_RESOURCE_FILES "${CMAKE_CURRENT_BINARY_DIR}/android_resources/mipmap-mdpi/ic_launcher.png")
+            list(APPEND ANDROID_RESOURCE_FILES "${CMAKE_CURRENT_BINARY_DIR}/android_resources/mipmap-hdpi/ic_launcher.png")
+            list(APPEND ANDROID_RESOURCE_FILES "${CMAKE_CURRENT_BINARY_DIR}/android_resources/mipmap-xhdpi/ic_launcher.png")
+            list(APPEND ANDROID_RESOURCE_FILES "${CMAKE_CURRENT_BINARY_DIR}/android_resources/mipmap-xxhdpi/ic_launcher.png")
+            list(APPEND ANDROID_RESOURCE_FILES "${CMAKE_CURRENT_BINARY_DIR}/android_resources/mipmap-xxxhdpi/ic_launcher.png")
+        endif()
+    endif()
+
     set(SP2_TARGET_NAME ${EXECUTABLE_NAME})
     if(ANDROID)
         # For Android, we need a libmain.so, which is packed into an android APK, and loaded from java sources
@@ -108,12 +176,66 @@ macro(serious_proton2_executable EXECUTABLE_NAME)
     endif()
 endmacro()
 
-if(WIN32)
-    set(CPACK_GENERATOR NSIS ZIP)
-    find_package(Git)
-    if(GIT_FOUND)
-        execute_process(COMMAND ${GIT_EXECUTABLE} describe --always OUTPUT_VARIABLE GIT_VERSION WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}" OUTPUT_STRIP_TRAILING_WHITESPACE)
-        set(CPACK_PACKAGE_VERSION "${GIT_VERSION}")
-    endif()
-    include(CPack)
-endif()
+# Macro to build the android apk
+macro(android_apk NAME ASSETS_FOLDER)
+    configure_file("${SP2_DIR}/android/AndroidManifest.xml.in" "${CMAKE_CURRENT_BINARY_DIR}/AndroidManifest.xml")
+    configure_file("${SP2_DIR}/android/SDLActivity.java.in" "${CMAKE_CURRENT_BINARY_DIR}/java_source/sp2/${NAME}/SDLActivity.java")
+    # Generate the R.java file
+    add_custom_command(
+        OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/java_source/sp2/${NAME}/R.java"
+        COMMAND "${AAPT}" ARGS package -m -J "${CMAKE_CURRENT_BINARY_DIR}/java_source" -M "${CMAKE_CURRENT_BINARY_DIR}/AndroidManifest.xml" -S "${CMAKE_CURRENT_BINARY_DIR}/android_resources" -I "${ANDROID_PLATFORM_JAR}"
+        DEPENDS "${CMAKE_CURRENT_BINARY_DIR}/AndroidManifest.xml" ${ANDROID_RESOURCE_FILES}
+    )
+    # Generate apk with resource files and manifest, but nothing else
+    # Compile the java sources (TODO, warning about bootstrap classpath, rt.jar wrong version)
+    file(GLOB JAVA_SOURCES "${SDL_SRC_PATH}/android-project/app/src/main/java/org/libsdl/app/*.java")
+    list(APPEND JAVA_SOURCES "${CMAKE_CURRENT_BINARY_DIR}/java_source/sp2/${NAME}/R.java")
+    list(APPEND JAVA_SOURCES "${CMAKE_CURRENT_BINARY_DIR}/java_source/sp2/${NAME}/SDLActivity.java")
+    file(MAKE_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/java_compiled/")
+    add_custom_command(
+        OUTPUT "java_compiled/sp2/Template/R.class"
+        COMMAND ${Java_JAVAC_EXECUTABLE} ARGS -source 1.7 -target 1.7 -classpath "${ANDROID_PLATFORM_JAR}" ${JAVA_SOURCES} -d "${CMAKE_CURRENT_BINARY_DIR}/java_compiled/"
+        DEPENDS ${JAVA_SOURCES}
+    )
+    # Convert sources into dex file
+    add_custom_command(
+        OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/apk_contents/classes.dex"
+        COMMAND "${DX}" ARGS "--dex" "--output=${CMAKE_CURRENT_BINARY_DIR}/apk_contents/classes.dex" "${CMAKE_CURRENT_BINARY_DIR}/java_compiled/"
+        DEPENDS "java_compiled/sp2/Template/R.class"
+    )
+
+    file(COPY "${SDL_INSTALL_PATH}/lib/libSDL2.so" DESTINATION "${CMAKE_CURRENT_BINARY_DIR}/apk_contents/lib/${ANDROID_ABI}/")
+    file(COPY "${SDL_INSTALL_PATH}/lib/libhidapi.so" DESTINATION "${CMAKE_CURRENT_BINARY_DIR}/apk_contents/lib/${ANDROID_ABI}/")
+    add_custom_command(
+        OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/apk_contents/lib/${ANDROID_ABI}/libmain.so"
+        COMMAND "${CMAKE_COMMAND}" ARGS -E copy "$<TARGET_FILE:main>" "${CMAKE_CURRENT_BINARY_DIR}/apk_contents/lib/${ANDROID_ABI}/libmain.so"
+        DEPENDS "$<TARGET_FILE:main>"
+    )
+
+    set(APK_ALIGNED "${CMAKE_CURRENT_BINARY_DIR}/${NAME}.apk")
+    set(APK "${APK_ALIGNED}.unaligned")
+    file(COPY "${ASSETS_FOLDER}" DESTINATION "${CMAKE_CURRENT_BINARY_DIR}/apk_contents/assets/")
+    file(GLOB_RECURSE ASSETS LIST_DIRECTORIES false RELATIVE "${CMAKE_CURRENT_BINARY_DIR}/apk_contents/" "${CMAKE_CURRENT_BINARY_DIR}/apk_contents/assets/*")
+    add_custom_command(
+        OUTPUT "${APK}"
+        COMMAND "${AAPT}" ARGS package -f -M "${CMAKE_CURRENT_BINARY_DIR}/AndroidManifest.xml" -S "${CMAKE_CURRENT_BINARY_DIR}/android_resources" -I "${ANDROID_PLATFORM_JAR}" -F "${APK}"
+        COMMAND "${AAPT}" ARGS add "${APK}" classes.dex
+        COMMAND "${AAPT}" ARGS add "${APK}" lib/${ANDROID_ABI}/libmain.so lib/${ANDROID_ABI}/libSDL2.so lib/${ANDROID_ABI}/libhidapi.so
+        COMMAND "${AAPT}" ARGS add "${APK}" ${ASSETS}
+        COMMAND "${Java_JARSIGNER_EXECUTABLE}" ARGS -verbose -sigalg SHA1withRSA -digestalg SHA1 -storepass "${ANDROID_SIGN_KEY_PASSWORD}" "${APK}" "${ANDROID_SIGN_KEY_NAME}"
+        WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/apk_contents/"
+        DEPENDS "${CMAKE_CURRENT_BINARY_DIR}/apk_contents/classes.dex" "${CMAKE_CURRENT_BINARY_DIR}/apk_contents/lib/${ANDROID_ABI}/libmain.so"
+    )
+    add_custom_command(
+        OUTPUT "${APK_ALIGNED}"
+        COMMAND "${CMAKE_COMMAND}" -E remove -f "${APK_ALIGNED}"
+        COMMAND "${ZIPALIGN}" ARGS 4 "${APK}" "${APK_ALIGNED}"
+        DEPENDS "${APK}"
+    )
+    add_custom_target(apk ALL DEPENDS "${APK_ALIGNED}")
+    add_custom_target(upload
+        COMMAND ${ADB} install -r "${APK_ALIGNED}"
+        COMMAND ${ADB} shell am start -n "sp2.${NAME}/.SDLActivity"
+        DEPENDS "${APK_ALIGNED}" WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}"
+    )
+endmacro()
