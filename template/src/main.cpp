96833a576262d931b10f17c9a1bfba978bb830fe
Daid <daid303@gmail.com>
2019-09-16 10:37:45 +0200

    Fix the template for the API change in the gui scene.

--- template/src/main.cpp
+++ template/src/main.cpp
@@ -35,7 +35,7 @@ int main(int argc, char** argv)
 #endif
 
     sp::gui::Theme::loadTheme("default", "gui/theme/basic.theme.txt");
-    new sp::gui::Scene(sp::Vector2d(640, 480), sp::gui::Scene::Direction::Horizontal);
+    new sp::gui::Scene(sp::Vector2d(640, 480));
 
     sp::P<sp::SceneGraphicsLayer> scene_layer = new sp::SceneGraphicsLayer(1);
     scene_layer->addRenderPass(new sp::BasicNodeRenderPass());
