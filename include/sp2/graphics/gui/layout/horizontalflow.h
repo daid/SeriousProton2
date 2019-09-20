4f7fd4f63839e8caba31a50c931048dd9db623a8
daid <daid303@gmail.com>
2019-09-20 10:52:35 +0200

    Add horizontal flow layout for gui

---
+++ include/sp2/graphics/gui/layout/horizontalflow.h
@@ -0,0 +1,18 @@
+#ifndef SP2_GRAPHICS_GUI_LAYOUT_HORIZONTAL_FLOW_H
+#define SP2_GRAPHICS_GUI_LAYOUT_HORIZONTAL_FLOW_H
+
+#include <sp2/graphics/gui/layout/layout.h>
+
+namespace sp {
+namespace gui {
+
+class HorizontalFlowLayout : public Layout
+{
+public:
+    virtual void update(P<Widget> container, Rect2d rect) override;
+};
+
+};//namespace gui
+};//namespace sp
+
+#endif//SP2_GRAPHICS_GUI_LAYOUT_HORIZONTAL_FLOW_H
---
+++ src/graphics/gui/layout/horizontalflow.cpp
@@ -0,0 +1,32 @@
+#include <sp2/graphics/gui/layout/horizontalflow.h>
+#include <sp2/graphics/gui/widget/widget.h>
+
+namespace sp {
+namespace gui {
+
+SP_REGISTER_LAYOUT("horizontalflow", HorizontalFlowLayout);
+
+void HorizontalFlowLayout::update(P<Widget> container, Rect2d rect)
+{
+    double x = rect.position.x;
+    double y0 = rect.position.y + rect.size.y;
+    double y1 = rect.position.y + rect.size.y;
+    for(P<Widget> w : container->getChildren())
+    {
+        if (!w || !w->isVisible())
+            continue;
+        double width = w->layout.size.x + w->layout.margin.left + w->layout.margin.right;
+        if (x + width > rect.size.x)
+        {
+            y0 = y1;
+            x = rect.position.x;
+        }
+        double height = y0 - rect.position.y;
+        basicLayout(Rect2d(x, y0 - height, width, height), *w);
+        y1 = std::min(y1, y0 - w->getRenderSize().y - w->layout.margin.bottom);
+        x += width;
+    }
+}
+
+};//namespace gui
+};//namespace sp
