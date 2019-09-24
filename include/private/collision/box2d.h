55bbfeb9a4a920276a25181b761037fbd249d720
Daid <daid303@gmail.com>
2019-09-24 12:33:18 +0000

    Only use the gcc specific pragmas if we actually have gcc.

--- include/private/collision/box2d.h
+++ include/private/collision/box2d.h
@@ -1,9 +1,13 @@
 #ifndef SP2_PRIVATE_COLLISION_BOX2D_H
 #define SP2_PRIVATE_COLLISION_BOX2D_H
 
+#if defined(__GNUC__) && !defined(__clang__)
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wsuggest-override"
+#endif//__GNUC__
 #include <Box2D/Box2D.h>
+#if defined(__GNUC__) && !defined(__clang__)
 #pragma GCC diagnostic pop
+#endif//__GNUC__
 
 #endif//SP2_PRIVATE_COLLISION_BOX2D_H
--- include/private/collision/bullet.h
+++ include/private/collision/bullet.h
@@ -1,9 +1,13 @@
 #ifndef SP2_PRIVATE_COLLISION_BULLET_H
 #define SP2_PRIVATE_COLLISION_BULLET_H
 
+#if defined(__GNUC__) && !defined(__clang__)
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wsuggest-override"
+#endif//__GNUC__
 #include <btBulletDynamicsCommon.h>
+#if defined(__GNUC__) && !defined(__clang__)
 #pragma GCC diagnostic pop
+#endif//__GNUC__
 
 #endif//SP2_PRIVATE_COLLISION_BULLET_H
