9dffc36183b53041e4c36d4578eece3335b0f195
daid <daid303@gmail.com>
2020-04-20 14:00:10 +0200

    Update color.cpp

--- src/graphics/color.cpp
+++ src/graphics/color.cpp
@@ -4,20 +4,20 @@ namespace sp {
 
 HsvColor::HsvColor(const sp::Color& color)
 {
-    int max = std::max(std::max(color.r, color.g), color.b);
-    int min = std::min(std::min(color.r, color.g), color.b);
+    auto max = std::max(std::max(color.r, color.g), color.b);
+    auto min = std::min(std::min(color.r, color.g), color.b);
     
-    int delta = max - min;
-    if (delta < 1)
+    auto delta = max - min;
+    if (delta =< 0.0)
     {
         hue = 0.0;
         saturation = 0.0;
-        value = double(max) * 100 / 255;
+        value = double(max) * 100;
         return;
     }
     hue = 0.0;
     saturation = double(delta) / double(max) * 100.0;
-    value = max * 100 / 255 + 0.5;
+    value = max * 100;
     if (color.r == max)
         hue = double(color.g - color.b) / double(max);
     else if (color.g == max)
