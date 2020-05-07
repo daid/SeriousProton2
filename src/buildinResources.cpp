8c965a26c955bc9b2935f7066246f28abb4e530d
Daid <daid303@gmail.com>
2020-05-07 16:19:17 +0200

    Add a color_by_normal shader, which is useful for coloring text.

--- src/buildinResources.cpp
+++ src/buildinResources.cpp
@@ -37,6 +37,45 @@ void main()
 }
 )EOS"},
 
+    {"color_by_normal.shader", R"EOS(
+[VERTEX]
+attribute vec3 a_vertex;
+attribute vec3 a_normal;
+attribute vec2 a_uv;
+
+uniform mat4 projection_matrix;
+uniform mat4 camera_matrix;
+uniform mat4 object_matrix;
+uniform vec3 object_scale;
+
+varying vec2 v_uv;
+varying vec3 v_normal;
+
+void main()
+{
+    gl_Position = projection_matrix * camera_matrix * object_matrix * vec4(a_vertex.xyz * object_scale, 1.0);
+    v_uv = a_uv.xy;
+    v_normal = a_normal;
+}
+
+[FRAGMENT]
+uniform sampler2D texture_map;
+uniform vec4 color;
+
+varying vec2 v_uv;
+varying vec3 v_normal;
+
+void main()
+{
+    gl_FragColor = texture2D(texture_map, v_uv) * color;
+    gl_FragColor.r *= v_normal.x;
+    gl_FragColor.g *= v_normal.y;
+    gl_FragColor.b *= v_normal.z;
+    if (gl_FragColor.a == 0.0)
+        discard;
+}
+)EOS"},
+
     {"basic_shaded.shader", R"EOS(
 [VERTEX]
 attribute vec3 a_vertex;
