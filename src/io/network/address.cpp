33941db2223e6eeef79c8f749aa193f59587d979
daid <daid303@gmail.com>
2022-05-16 17:36:53 +0200

    Update address.cpp

--- src/io/network/address.cpp
+++ src/io/network/address.cpp
@@ -105,7 +105,7 @@ Address Address::getLocalAddress()
             if ((addr->ifa_flags & IFF_LOOPBACK) || !(addr->ifa_flags & IFF_UP))
                 continue;
             if (addr->ifa_addr == nullptr)
-                continue
+                continue;
             if (addr->ifa_addr->sa_family == AF_INET)
             {
                 ::getnameinfo(addr->ifa_addr, sizeof(struct sockaddr_in), buffer, sizeof(buffer), nullptr, 0, NI_NUMERICHOST);
