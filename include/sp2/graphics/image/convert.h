1b410abdd66f2ef011c662fd6c52302b58cffd1c
Daid <daid303@gmail.com>
2023-11-05 09:29:46 +0100

    fix image 8 to 32 bit conversion

--- include/sp2/graphics/image/convert.h
+++ include/sp2/graphics/image/convert.h
@@ -7,7 +7,7 @@
 namespace sp::image {
 
 namespace detail {
-    template<typename DST, typename SRC> struct PixelConvertInfo;
+    template<typename SRC, typename DST> struct PixelConvertInfo;
     template<> struct PixelConvertInfo<uint32_t, uint8_t> { static inline uint8_t conv(uint32_t in) { return ((in & 0xFF) + ((in >> 8) & 0xFF) + ((in >> 16) & 0xFF)) / 3; } };
     template<> struct PixelConvertInfo<uint8_t, uint32_t> { static inline uint32_t conv(uint8_t in) { return 0xFF000000 | in | (in << 8) | (in << 16); } };
 }
@@ -17,7 +17,7 @@ template<typename DST, typename SRC> ImageBase<DST> convert(const ImageBase<SRC>
     auto src = input.getPtr();
     auto dst = output.getPtr();
     for(int n=0; n<input.getSize().x*input.getSize().y; n++)
-        *dst++ = detail::PixelConvertInfo<DST, SRC>::conv(*src++);
+        *dst++ = detail::PixelConvertInfo<SRC, DST>::conv(*src++);
     return output;
 }
 
